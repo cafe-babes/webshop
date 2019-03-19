707c16c52b3f4109c8ee121c38b8ca9722e55991
ggerst <gerstg@gmail.com>
2019-03-19 17:44:05 +0100

    spring boot initial

---
+++ src/main/java/com/training360/cafebabeswebshop/CafebabeswebshopApplication.java
@@ -0,0 +1,13 @@
+package com.training360.cafebabeswebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafebabeswebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafebabeswebshopApplication.class, args);
+	}
+
+}
