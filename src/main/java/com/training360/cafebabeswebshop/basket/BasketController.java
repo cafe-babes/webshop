50422aa114aec0512cdee051065fdf213b281b7c
ggerst <gerstg@gmail.com>
2019-03-20 17:50:18 +0100

    basket v0.1

--- src/main/java/com/training360/cafebabeswebshop/basket/BasketController.java
+++ src/main/java/com/training360/cafebabeswebshop/basket/BasketController.java
@@ -1,7 +1,18 @@
 package com.training360.cafebabeswebshop.basket;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasketController {
+
+    @Autowired
+    private BasketService basketService;
+
+    @PostMapping
+    public long saveBasketItemAndGetId(@RequestBody Basket basket) {
+        return basketService.saveBasketItemAndGetId(basket);
+    }
 }
