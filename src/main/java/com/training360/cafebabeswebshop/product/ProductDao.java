8ee3e1aaf5f579d1f929a1c9d443b9ce00978e08
noraanovakk <noraanovakk@gmail.com>
2019-03-20 14:02:25 +0100

    half done list

--- src/main/java/com/training360/cafebabeswebshop/product/ProductDao.java
+++ src/main/java/com/training360/cafebabeswebshop/product/ProductDao.java
@@ -41,5 +41,13 @@ public class ProductDao {
         }
     }
 
-
+    public List<Product> getProducts(){
+        return jdbcTemplate.query("select id, code, address, name, manufacture, price from products", new RowMapper<Product>() {
+            @Override
+            public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Product(resultSet.getLong("id"), resultSet.getString("code"), resultSet.getString("address"),
+                        resultSet.getString("name"), resultSet.getString("manufacture"), resultSet.getInt("price"));
+            }
+        });
+    }
 }
