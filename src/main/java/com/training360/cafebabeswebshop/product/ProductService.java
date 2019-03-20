306fd5bfe72999d3f466cb9ac98d6c9597af8319
Rekasi Andrea <rekasi.anci@gmail.com>
2019-03-20 10:40:58 +0100

    story 1 dao+service

--- src/main/java/com/training360/cafebabeswebshop/product/ProductService.java
+++ src/main/java/com/training360/cafebabeswebshop/product/ProductService.java
@@ -1,4 +1,19 @@
 package com.training360.cafebabeswebshop.product;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+
+    private ProductDao productDao;
+
+    public ProductService(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public Product getProduct(Product product){
+        return productDao.getProduct(product);
+    }
 }
--- src/main/java/com/training360/cafebabeswebshop/products/Product.java
+++
@@ -1,6 +0,0 @@
-package com.training360.cafebabeswebshop.products;
-
-public class Product {
-
-
-}
