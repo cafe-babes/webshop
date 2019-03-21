68aaa3c6bfa50d43c938fd40edb99c1825675c21
noraanovakk <noraanovakk@gmail.com>
2019-03-21 14:46:58 +0100

    get right product controller

--- src/test/java/com/training360/cafebabeswebshop/CafebabeswebshopApplicationTests.java
+++ src/test/java/com/training360/cafebabeswebshop/CafebabeswebshopApplicationTests.java
@@ -1,16 +1,24 @@
-package com.training360.cafebabeswebshop;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CafebabeswebshopApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.training360.cafebabeswebshop;
+//
+//import com.training360.cafebabeswebshop.product.ProductController;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.jdbc.Sql;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Sql(scripts = "/init.sql")
+//public class CafebabeswebshopApplicationTests {
+//
+//	@Autowired
+//	private ProductController productController;
+//
+//	@Test
+//	public void contextLoads() {
+//		productController.
+//	}
+//
+//}
