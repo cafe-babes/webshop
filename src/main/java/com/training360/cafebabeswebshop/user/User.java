eb3a84b8ea4769b96b78003e541b1b180a883b7e
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-21 16:23:24 +0100

    users back-end userUpdate finished

--- src/main/java/com/training360/cafebabeswebshop/user/User.java
+++ src/main/java/com/training360/cafebabeswebshop/user/User.java
@@ -9,15 +9,17 @@ public class User {
     private String email;
     private String user_name;
     private String password;
+    private int enabled;
     private String role;
     private String user_status;
 
-    public User(long id, String name, String email, String user_name, String password, String role, String user_status) {
+    public User(long id, String name, String email, String user_name, String password, int enabled, String role, String user_status) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.user_name = user_name;
         this.password = new BCryptPasswordEncoder(4).encode(password);
+        this.enabled = enabled;
         this.role = role;
         this.user_status = user_status;
     }
@@ -48,5 +50,9 @@ public class User {
 
     public String getUser_status() {
         return user_status;
+    }
+
+    public int getEnabled() {
+        return enabled;
     }
 }
