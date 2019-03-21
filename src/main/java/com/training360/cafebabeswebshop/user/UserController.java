00064a2c8ecf5f11102ce824da0721058ad61b4d
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-21 17:23:01 +0100

    users back-end userUpdate finished

--- src/main/java/com/training360/cafebabeswebshop/user/UserController.java
+++ src/main/java/com/training360/cafebabeswebshop/user/UserController.java
@@ -4,6 +4,7 @@ import com.training360.cafebabeswebshop.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,6 +12,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    UserValidator userValidator;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -32,10 +35,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public long insertUserAndGetId(@RequestBody User user) {
-        return userService.insertUserAndGetId(user);
+    public ResultStatus insertUserAndGetId(@RequestBody User user) {
+        userValidator = new UserValidator(userService);
+            if(userValidator.userCanBeSaved(user)){
+                long id = userService.insertUserAndGetId(user);
+                return new ResultStatus(ResultStatusEnum.OK, "Felhasználó sikeresen elmentve");
+            }
+        return new ResultStatus(ResultStatusEnum.NOT_OK, "Ez a felhasználó már regisztrált!");
     }
-
-
 
 }
