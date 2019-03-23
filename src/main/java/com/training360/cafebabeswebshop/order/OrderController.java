4d7bc8706ee9cb1e1401c326b506b6ceac9760fc
Rekasi Andrea <rekasi.anci@gmail.com>
2019-03-23 16:52:16 +0100

    order csr majdnem minden tökéletes

--- src/main/java/com/training360/cafebabeswebshop/order/OrderController.java
+++ src/main/java/com/training360/cafebabeswebshop/order/OrderController.java
@@ -1,14 +1,12 @@
 package com.training360.cafebabeswebshop.order;
 
+import com.training360.cafebabeswebshop.product.ProductService;
 import com.training360.cafebabeswebshop.product.ResultStatus;
 import com.training360.cafebabeswebshop.product.ResultStatusE;
 import com.training360.cafebabeswebshop.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,12 +17,15 @@ public class OrderController {
     private OrderService orderService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private ProductService productService;
     private OrderValidator validator;
 
-    public OrderController(OrderService orderService, UserService userService) {
+    public OrderController(OrderService orderService, UserService userService, ProductService productService) {
         this.orderService = orderService;
         this.userService = userService;
-        this.validator = new OrderValidator(userService);
+        this.productService = productService;
+        this.validator = new OrderValidator(orderService,userService,productService);
     }
 
     @PostMapping("/myorders")
@@ -44,6 +45,36 @@ public class OrderController {
     @GetMapping("/myorders")
     public List<Order> listMyOrders(Authentication authentication){
         return orderService.listMyOrders(authentication);
+    }
+
+    @GetMapping("/orders")
+    public List<Order> listAllOrders(){
+        return orderService.listAllOrders();
+    }
+
+    @GetMapping("/orders/{id}")
+    public List<OrderedProduct> listOrderedProductsByOrderId(@PathVariable long id){
+        return orderService.listOrderedProductsByOrderId(id);
+    }
+
+    @PostMapping("/orders/{id}")
+    public ResultStatus deleteOrder(@PathVariable long id){
+        if (validator.isValidOrderId(id)){
+            orderService.deleteOrder(id);
+            return new ResultStatus(ResultStatusE.OK,String.format("Order successfully deleted with id %d", id));
+        } else {
+            return new ResultStatus(ResultStatusE.NOT_OK, "Invalid id");
+        }
+    }
+
+    @DeleteMapping("/orders/{id}/{address}")
+    public ResultStatus deleteOneItemFromOrder(@PathVariable long id, @PathVariable String address){
+        if (validator.isExistingOrderId(id) && validator.isExistingProductAddress(address)){
+            orderService.deleteOneItemFromOrder(id,address);
+            return new ResultStatus(ResultStatusE.OK, "Ordered product deleted successfully");
+        } else {
+            return new ResultStatus(ResultStatusE.NOT_OK, "Invalid id, or address");
+        }
     }
 
 }
