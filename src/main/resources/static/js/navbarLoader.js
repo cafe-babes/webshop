299dbb1c2414aef65471a4340507ab6db4b279fd
ggerst <gerstg@gmail.com>
2019-03-24 15:06:38 +0100

    frontend navbar szépítés, csoportosítás

--- src/main/resources/static/js/navbarLoader.js
+++ src/main/resources/static/js/navbarLoader.js
@@ -1,13 +1,13 @@
 $.getJSON("/user-role", json => {
 
-let role = json.role;
-let name = json.userName;
+    let role = json.role || 'VISITOR';
+    let name = json.userName;
 
-document.querySelector('nav').innerHTML =
-    `<a class="navbar-brand" href="/">CafeBabes</a>
+    document.querySelector('nav').innerHTML =
+        `<a class="navbar-brand navbar-inverse" href="/">CafeBabes</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-           aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
+        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarColor01">
@@ -15,44 +15,55 @@ document.querySelector('nav').innerHTML =
        </ul>
     </div>`;
 
-let navFill = document.querySelector('#variable-navbar');
+    let navFill = document.querySelector('#variable-navbar');
 
-navFill.innerHTML +=
-           `<li class="nav-item active" id="home">
-               <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
-           </li>`;
+    navFill.innerHTML +=
+        `<li class="nav-item active" id="home">
+            <a class="nav-link" href="/">Home</a>
+        </li>`;
 
-if(role == 'ADMIN'){
-navFill.innerHTML +=
-        `<li class="nav-item active" id="admin">
-            <a class="nav-link" href="/adminproducts.html">Termékek adminisztrációja<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item active" id="users">
-            <a class="nav-link" href="/users.html">Felhasználók adminisztrációja<span class="sr-only">(current)</span></a>
-        </li>`
-}
+    if (role == 'ADMIN') {
+        navFill.innerHTML +=
+            `<div class="btn-group">
+            <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin menü<span
+                    class="sr-only">Toggle Dropdown</span>
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="/adminproducts.html">Termékek adminisztrációja</a>
+                <a class="dropdown-item" href="/users.html">Felhasználók adminisztrációja</a>
+                <a class="dropdown-item" href="/orders.html">Rendelések adminisztrációja</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="/dashboard.html">Dashboard</a>
+            </div>
+        </div>`
+    }
 
-if(role == 'VISITOR') {
-navFill.innerHTML +=
-        `<li class="nav-item active" id="sign-up">
-            <a class="nav-link" href="/register.html">Sign Up<span class="sr-only">(current)</span></a>
+    if (role == 'VISITOR') {
+        navFill.innerHTML +=
+            `<li class="nav-item active" id="sign-up">
+            <a class="nav-link" href="/register.html">Sign Up</a>
         </li>
         <li class="nav-item active" id="login">
-            <a class="nav-link" href="/login">Log in<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/login">Log in</a>
         </li>`
-} else {
-navFill.innerHTML +=
-        `<li class="nav-item active" id="logout">
-            <a class="nav-link" href="/logout">Log out<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item" id="full-name">
-            <a class="nav-link disabled">Üdvözlünk ${name}!<span class="sr-only">(current)</span></a>
-        </li>`
-}
+    } else {
+        navFill.innerHTML +=
+            `<div class="btn-group">
+            <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Üdvözlünk ${name}!<span
+                    class="sr-only">Toggle Dropdown</span>
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="/orders.html">Rendeléseim</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="/logout">Log out</a>
+            </div>
+        </div>`
+    }
 
-navFill.innerHTML +=
-      `<li class="nav-item active" id="basket">
-           <a class="nav-link" href="basket.html"><i class="fas fa-shopping-cart"></i></a>
-       </li>`;
+    navFill.innerHTML +=
+        `<li class="nav-item active" id="basket">
+            <a class="nav-link" href="basket.html"><i class="fas fa-shopping-cart"></i></a>
+        </li>`;
 });
-
