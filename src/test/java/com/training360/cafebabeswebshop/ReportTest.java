bb5885e52879827ddb32dbeb62f732912144617b
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-26 11:57:13 +0100

    test init

---
+++ src/test/java/com/training360/cafebabeswebshop/ReportTest.java
@@ -0,0 +1,33 @@
+package com.training360.cafebabeswebshop;
+
+import com.training360.cafebabeswebshop.order.Order;
+import com.training360.cafebabeswebshop.order.OrderController;
+import com.training360.cafebabeswebshop.user.User;
+import com.training360.cafebabeswebshop.user.UserController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Sql(scripts = "/init.sql")
+public class ReportTest {
+
+        @Test
+    public void testGetMonthlyIncomeOfOrders(){
+
+        }
+
+}
