99010d6a9a0a76a8392a60273cd45303f978d3e4
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-26 17:42:00 +0100

    More products

--- src/main/resources/db/migration/V1__create_webshop_tables.sql
+++ src/main/resources/db/migration/V1__create_webshop_tables.sql
@@ -33,3 +33,12 @@ INSERT INTO products (`code`, `address`, `name`, `manufacture`, `price`, `produc
 VALUES ('GH672V4', 'shark', 'Shark', 'cafebabes', 97000, 'ACTIVE');
 INSERT INTO products (`code`, `address`, `name`, `manufacture`, `price`, `product_status`)
 VALUES ('DB89AS1', 'blowfish', 'Blow Fish', 'cafebabes', 112000, 'ACTIVE');
+INSERT INTO products (id, `code`, `address`, `name`, `manufacture`, `price`, `product_status`) VALUES
+(11, 'SK001', 'surfer', 'Surfer', 'Pacific Islands Surfboards', 990000, 'ACTIVE'),
+(12, 'R2001', 'R2', 'R2', 'Chemistry Surfboards', 880000, 'ACTIVE'),
+(13, 'SA001', 'sampler', 'Sampler', 'Channel Islands', 45000, 'ACTIVE'),
+(14, 'BA001', 'bastard', 'Bastard', 'Pyzel', 45000, 'DELETED'),
+(15, 'GB001', 'greedy_beaver', 'Greedy Beaver', 'Firewire', 45000, 'ACTIVE'),
+(16, 'JB101', 'jb-1', 'JB-1', 'T. Patterson', 45000, 'ACTIVE'),
+(17, 'UT001', 'utility', 'Utility', 'Rusty Surfboards', 45000, 'DELETED'),
+(18, 'DI001', 'deserted_island', 'Deserted Island', 'Blue Sea Watersports', 45000, 'ACTIVE');
