42b490f70be4ad6200b8ae4e7773b44ca69e539c
ggerst <gerstg@gmail.com>
2019-03-29 15:13:15 +0100

    user-role ad már id-t és categories admin félkész

--- src/main/java/com/training360/cafebabeswebshop/product/ProductValidator.java
+++ src/main/java/com/training360/cafebabeswebshop/product/ProductValidator.java
@@ -21,7 +21,7 @@ public class ProductValidator {
         return (str != null && !str.trim().equals(""));
     }
 
-    private boolean isValidPrice(int price){
+    public boolean isValidPrice(int price){
         return (price > 0 && price <= 2_000_000);
     }
 }
--- src/main/java/com/training360/cafebabeswebshop/user/UserDao.java
+++ src/main/java/com/training360/cafebabeswebshop/user/UserDao.java
@@ -63,6 +63,12 @@ public class UserDao {
     }
 
     public User getUserById(long id) {
-        return jdbcTemplate.queryForObject("select `id`, `name`, `email`, `user_name`, `password`, `enabled`, `role`, `user_status` FROM `users` where id = ? ", USER_ROW_MAPPER, id);
+        return jdbcTemplate.queryForObject("select id, name, email, user_name, password, enabled, role, user_status FROM users WHERE id = ? ",
+                USER_ROW_MAPPER, id);
+    }
+
+    public User getUserByName(String userName) {
+        return jdbcTemplate.queryForObject("select id, name, email, user_name, password, enabled, role, user_status FROM users WHERE user_name = ? ",
+                USER_ROW_MAPPER, userName);
     }
 }
