e35b9f45f7e4e5a891ef60a2f32209cb53508c37
Rekasi Andrea <rekasi.anci@gmail.com>
2019-03-29 17:03:08 +0100

    more items in the basket

---
+++ src/main/resources/db/migration/V14__alter_ordered_products.sql
@@ -0,0 +1,5 @@
+UPDATE ordered_products set pieces = 1 where product_id = 10;
+UPDATE ordered_products set pieces = 1 where product_id = 5;
+UPDATE ordered_products set pieces = 1 where product_id = 2;
+UPDATE ordered_products set pieces = 1 where product_id = 8;
+UPDATE ordered_products set pieces = 1 where product_id = 7;
