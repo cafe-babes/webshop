9e3b042e196864985498b8976432b9db509eff89
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-29 08:14:52 +0100

    FeedbackTest getfeedbackByProductId

---
+++ src/test/java/com/training360/cafebabeswebshop/FeedbackTest.java
@@ -0,0 +1,26 @@
+package com.training360.cafebabeswebshop;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Sql(scripts = "/init.sql")
+public class FeedbackTest {
+
+@Test
+    public void getFeedbackByProductId(){
+
+}
+
+
+}
