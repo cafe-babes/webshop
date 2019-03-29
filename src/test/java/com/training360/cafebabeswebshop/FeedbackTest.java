1923ebef1c71b13c7fe80f098069a1e4bd16176e
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-29 08:28:43 +0100

    FeedbackTest giveAFeedback

--- src/test/java/com/training360/cafebabeswebshop/FeedbackTest.java
+++ src/test/java/com/training360/cafebabeswebshop/FeedbackTest.java
@@ -1,4 +1,9 @@
 package com.training360.cafebabeswebshop;
+import com.training360.cafebabeswebshop.category.Category;
+import com.training360.cafebabeswebshop.feedback.Feedback;
+import com.training360.cafebabeswebshop.feedback.FeedbackService;
+import com.training360.cafebabeswebshop.product.Product;
+import com.training360.cafebabeswebshop.user.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +22,29 @@ import static org.junit.Assert.assertEquals;
 @Sql(scripts = "/init.sql")
 public class FeedbackTest {
 
+    @Autowired
+    FeedbackService feedbackService;
+
 @Test
-    public void getFeedbackByProductId(){
+    public void testListFeedBacksByProductId(){
+
+    List<Feedback> feedbacks = feedbackService.listFeedBacksByProductId(1);
+    assertEquals(3,feedbacks.get(0).getUser().getId());
+    assertEquals("Never a better shop!",feedbacks.get(0).getFeedback());
 
 }
+
+//    @Test
+//    public void testGiveAFeedBack(){
+//
+//        User user = new User(5,"Ernő","alma@alma.com","ernőke","ernőő",
+//                1,"ROLE_ADMIN","ACTIVE");
+//        Category category = new Category(5,"balzsamecet",2);
+//        Product product = new Product(99,"sss","sssss","sssssssss","aaaaaa",
+//                500000,"ACTIVE",category);
+//        feedbackService.giveAFeedback(new Feedback(5,"Awesome!",5,user,
+//                product));
+//    }
 
 
 }
