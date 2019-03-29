f61a91e25eca22573f232fc01645f654e3fa37a3
ggerst <gerstg@gmail.com>
2019-03-29 17:03:53 +0100

    order varia félkéész

--- src/main/java/com/training360/cafebabeswebshop/order/OrderDao.java
+++ src/main/java/com/training360/cafebabeswebshop/order/OrderDao.java
@@ -21,8 +21,8 @@ public class OrderDao {
             rs.getLong("id"),
             rs.getTimestamp("purchase_date").toLocalDateTime(),
             rs.getLong("user_id"),
-            rs.getLong("total"),
-            rs.getLong("sum_quantity"),
+            rs.getLong("sum(ordering_price*pieces)"),
+            rs.getLong("sum(pieces)"),
             rs.getString("order_status")
     );
     private static final RowMapper<OrderedProduct> ORDERED_PRODUCT_ROW_MAPPER = (rs, rowNum) -> new OrderedProduct(
@@ -34,17 +34,6 @@ public class OrderDao {
             rs.getInt("pieces")
     );
 
-    private static final RowMapper<User> USER_ROW_MAPPER = (rs, rowNum) -> new User(
-            rs.getLong("id"),
-            rs.getString("name"),
-            rs.getString("email"),
-            rs.getString("user_name"),
-            rs.getString("password"),
-            rs.getInt("enabled"),
-            rs.getString("role"),
-            rs.getString("user_status")
-    );
-
     public OrderDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -52,28 +41,24 @@ public class OrderDao {
     public long saveOrderAndGetId(String userName, Order order) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement("insert into orders (purchase_date, user_id, total, sum_quantity) " +
-                            "values (?,(SELECT id FROM users WHERE user_name = ?),?,?)",
+            PreparedStatement ps = connection.prepareStatement("insert into orders (purchase_date, user_id) " +
+                            "values (?,(SELECT id FROM users WHERE user_name = ?))",
                     Statement.RETURN_GENERATED_KEYS);
             ps.setTimestamp(1, Timestamp.valueOf(order.getPurchaseDate()));
             ps.setString(2, userName);
-            ps.setLong(3, order.getTotal());
-            ps.setLong(4, order.getSumQuantity());
             // ps.setString(5, String.valueOf(order.getOrderStatus()));
             return ps;
         }, keyHolder);
         return keyHolder.getKey().longValue();
     }
 
-    public long getUserId(String userName) {
-        User u = jdbcTemplate.queryForObject("select id, name, email, user_name, password, enabled, role, user_status " +
-                "from users where user_name = ?", USER_ROW_MAPPER, userName);
-        return u.getId();
-    }
-
     public Order findOrderById(long id) {
-        return jdbcTemplate.queryForObject("select id, purchase_date, user_id, total, sum_quantity, order_status from orders where id = ?",
-                ORDER_ROW_MAPPER, id);
+        return jdbcTemplate.queryForObject("SELECT id, purchase_date, user_id, " +
+                        "(SELECT sum(ordering_price) FROM ordered_products WHERE order_id = ?)," +
+                        "(SELECT sum(pieces) FROM ordered_products WHERE order_id = ?), " +
+                        "order_status " +
+                        "FROM orders WHERE id = ?",
+                ORDER_ROW_MAPPER, id, id, id);
     }
 
     public List<Order> listMyOrders(String username) {
@@ -124,15 +109,6 @@ public class OrderDao {
         return jdbcTemplate.queryForObject("select ordered_products.id, product_id, order_id, ordering_price, ordering_name, pieces" +
                         " from ordered_products join products on product_id = products.id where products.address = ? AND order_id = ?",
                 ORDERED_PRODUCT_ROW_MAPPER, address, orderId);
-    }
-
-    public void reduceOrderQuantityAndPriceWhenDeleting(long orderId, String address) throws DataAccessException {
-        Order o = findOrderById(orderId);
-        OrderedProduct op = findOrderedProductByProductAddress(orderId, address);
-        long newSumQuantity = o.getSumQuantity() - 1;
-        long newTotal = o.getTotal() - op.getOrderingPrice();
-        jdbcTemplate.update("update orders set sum_quantity = ? where id = ?", newSumQuantity, orderId);
-        jdbcTemplate.update("update orders set total = ? where id = ?", newTotal, orderId);
     }
 
     public void deleteOrder(long id) {
