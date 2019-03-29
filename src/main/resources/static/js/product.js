ee5d5bf177d320dfe53edaa6e38ca2f5cfefd4fd
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-29 20:09:30 +0100

    Feedback front-end

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,185 +1,184 @@
-window.onload = function() {
-    fetchProduct();
-}
-console.log((new URL(document.location)).searchParams.get("address"));
+window.onload = function () {
+  fetchProduct();
+};
+console.log((new URL(document.location)).searchParams.get('address'));
 
-$.getJSON("/user-role", json => {
-if(json.role != 'VISITOR')
+$.getJSON('/user-role', json => {
+  if (json.role != 'VISITOR') {
     document.querySelector('#addToBasketButton').hidden = false;
+  }
 });
 
-function fetchProduct(){
-var address = (new URL(document.location)).searchParams.get("address");
-var url = "/product/"+address;
-    fetch(url)
-    .then(function(response){
-    return response.json();
+function fetchProduct() {
+  var address = (new URL(document.location)).searchParams.get('address');
+  var url = '/product/' + address;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(jsonData) {
-        if (jsonData.status == 'NOT_OK') {
-           showProductNotFound(jsonData);
-        }else{
+    .then(function (jsonData) {
+      if (jsonData.status == 'NOT_OK') {
+        showProductNotFound(jsonData);
+      } else {
         console.log(jsonData);
-                        var productId = jsonData.id;
-                        fetchFeedback(productId);
-           showProduct(jsonData);
-        }
-});
-    return false;
+        var productId = jsonData.id;
+        fetchFeedback(productId);
+        showProduct(jsonData);
+      }
+    });
+  return false;
 }
 
 
 function fetchFeedback(productId) {
-    var url = "/feedback/" + productId;
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (jsonData) {
-            console.log("fetch ok");
-            console.log(jsonData);
-            showReviews(jsonData);
-        });
+  var url = '/feedback/' + productId;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      console.log('fetch ok');
+      console.log(jsonData);
+      showReviews(jsonData);
+    });
 }
 
 
 function showReviews(jsonData) {
+  var reviews = document.getElementById('reviews');
+  reviews.innerHTML = '';
 
-    var reviews = document.getElementById("reviews");
-    reviews.innerHTML = "";
-
-    for (var i = 0; i < jsonData.length; i++) {
-        reviews.innerHTML += `
+  for (var i = 0; i < jsonData.length; i++) {
+    reviews.innerHTML += `
                 <div class="container">
-                            <div>
-                                <div id="carousel-reviews" class="carousel slide" data-ride="carousel">
-                                    <div class="carousel-inner">
-                                        <div class="item active">
-                                            <div class="col-md-4 col-sm-6">
-                                                <div class="block-text rel zmin">
-                                                <small class="text-muted">Dátum: ${jsonData[i].feedbackDate.replace("T"," ")}</small>
-                                                    <div class="mark">My rating: <span class="rating-input"><span data-value="0"
-                                                                                                                  class="glyphicon glyphicon-star"></span><span
-                                                            data-value="1" class="glyphicon glyphicon-star"></span><span data-value="2"
-                                                                                                                         class="glyphicon glyphicon-star"></span><span
-                                                            data-value="3" class="glyphicon glyphicon-star"></span><span data-value="4"
-                                                                                                                         class="glyphicon glyphicon-star-empty"></span><span
-                                                            data-value="5" class="glyphicon glyphicon-star-empty"></span>  </span></div>
-                                                    <p>${jsonData[i].feedback}</p>
-                                                    <ins class="ab zmin sprite sprite-i-triangle block"></ins>
-                                                </div>
-                                                <br>
-                                                <div class="person-text rel">
-                                                    <a title="" href="#">${jsonData[i].user.name}</a>
-                                                </div>
-                                                </div>
-                                                </div>
-                                                </div>
-                                                </div>
-                                                </div>
-                                                </div>
+                <div>
+                    <div id="carousel-reviews" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="block-text rel zmin">
+                                    <small class="text-muted">Dátum: ${jsonData[i].feedbackDate.replace('T', ' ')}</small>
+                                        <div class="mark">My rating: <span class="rating-input"><span data-value="0"
+                                         class="glyphicon glyphicon-star"></span><span
+                                         data-value="1" class="glyphicon glyphicon-star"></span><span data-value="2"
+                                         class="glyphicon glyphicon-star"></span><span
+                                         data-value="3" class="glyphicon glyphicon-star"></span><span data-value="4"
+                                         class="glyphicon glyphicon-star-empty"></span><span
+                                         data-value="5" class="glyphicon glyphicon-star-empty"></span>  </span></div>
+                                        <p>${jsonData[i].feedback}</p>
+                                        <ins class="ab zmin sprite sprite-i-triangle block"></ins>
+                                    </div>
+                                    <br>
+                                    <div class="person-text rel">
+                                    <a title="" href="#">${jsonData[i].user.name}</a>
+                                </div>
+                            </div>
+                         </div>
+                     </div>
+                </div>
+           </div>
+      </div>
 
 
                 `;
-
-    }
-
-
-
+  }
 }
 
 
-function handleAddToBasketButton(){
-    var address = (new URL(document.location)).searchParams.get("address");
-    var url = "/basket";
-    fetch(url,{
-       method: "GET"
+function handleAddToBasketButton() {
+  var address = (new URL(document.location)).searchParams.get('address');
+  var url = '/basket';
+  fetch(url, {
+    method: 'GET'
+  })
+    .then(function (response) {
+      console.log(response);
+      if (response.ok == false) {
+        window.location.href = '/login';
+      } else {
+        return response.json();
+      }
     })
-    .then(function(response){
-    console.log(response);
-        if(response.ok==false)
-            window.location.href = "/login";
-        else
-            return response.json();
-    })
-    .then(function(jsonData) {
-        for(var i = 0; i < jsonData.length; i++) {
-            if(jsonData[i].address==address) {
-                alert("A termék már a kosárban van!");
-                return;
-            }
+    .then(function (jsonData) {
+      for (var i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].address == address) {
+          alert('A termék már a kosárban van!');
+          return;
         }
-        addToBasket();
+      }
+      addToBasket();
     });
 }
 
-function addToBasket(){
-    var quantity = document.querySelector("#inputQuantity").value;
-    var address = (new URL(document.location)).searchParams.get("address");
-    var url = "/basket/"+address;
+function addToBasket() {
+  var quantity = document.querySelector('#inputQuantity').value;
+  var address = (new URL(document.location)).searchParams.get('address');
+  var url = '/basket/' + address;
 
-    var request = {
-        "pieces": quantity
+  var request = {
+    'pieces': quantity
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers: {
+      'Content-type': 'application/json'
     }
-
-    fetch(url,{
-      method: "POST",
-      body: JSON.stringify(request),
-       headers: {
-           "Content-type": "application/json"
-       }
-      })
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(jsonData){
-        if(jsonData!=-1) {
-            addGoToBasketButton();
-            basketRefresh();
-        } 
-      })
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      if (jsonData != -1) {
+        addGoToBasketButton();
+        basketRefresh();
+      }
+    });
 }
 
-function addGoToBasketButton(){
-    var container = document.querySelector("#basketButton");
-    container.innerHTML = "";
-    container.innerHTML +=
-    `<a href="basket.html"><button type="button" class="btn btn-primary btn-lg">Irány a kosár</button></a>`;
+function addGoToBasketButton() {
+  var container = document.querySelector('#basketButton');
+  container.innerHTML = '';
+  container.innerHTML +=
+        '<a href="basket.html"><button type="button" class="btn btn-primary btn-lg">Irány a kosár</button></a>';
 }
 
 
 function showProduct(jsonData) {
+  var code = document.getElementById('code');
+  var name = document.getElementById('name');
+  var manufacture = document.getElementById('manufacturer');
+  var price = document.getElementById('price');
+  var reviewButton = document.getElementById('review-button');
+  reviewButton.innerHTML = '<a href = "#newReviewTarget" ><button type="button" class="btn btn-outline-primary" id="new-review" onclick="newReview()">Vélemény írása</button></a>';
 
-    var code = document.getElementById("code");
-    var name = document.getElementById("name");
-    var manufacture = document.getElementById("manufacturer");
-    var price = document.getElementById("price");
-    var reviewButton = document.getElementById("review-button");
-    reviewButton.innerHTML = `<a href = "#newReviewTarget" ><button type="button" class="btn btn-outline-primary" id="new-review" onclick="newReview()">Vélemény írása</button></a>`
-
-    code.innerHTML = jsonData.code;
-    name.innerHTML = jsonData.name;
-    manufacture.innerHTML = jsonData.manufacture;
-    price.innerHTML = jsonData.price;
-
+  code.innerHTML = jsonData.code;
+  name.innerHTML = jsonData.name;
+  manufacture.innerHTML = jsonData.manufacture;
+  price.innerHTML = jsonData.price;
 }
 
-function newReview(){
-
+function newReview() {
+var reviews = document.getElementById('reviews');
+reviews += `
+<div class="container">
+           <input id="nameInput${num}" type='text' minLength='1' maxLength='255' class='input-box' required>
+          </div>
 }
 
-function showProductNotFound(jsonData){
-    var productText = document.getElementById("product-text");
-    var pageNotFound = document.getElementById("page-not-found");
-    var picture = document.getElementById("picture");
-    var reviews = document.getElementById("reviews");
-    pageNotFound.innerHTML = ` <br>
+function showProductNotFound(jsonData) {
+  var productText = document.getElementById('product-text');
+  var pageNotFound = document.getElementById('page-not-found');
+  var picture = document.getElementById('picture');
+  var reviews = document.getElementById('reviews');
+  pageNotFound.innerHTML = ` <br>
                                 <div class="d-flex justify-content-center">
                                 <h2>Sajnos ilyen termékkel nem rendelkezünk...</h2>
                                 </div><br>
                                 <div class="d-flex justify-content-center"><img src="https://vignette.wikia.nocookie.net/kenny-the-shark/images/2/24/KTS_Gallery_570x402_08.jpg/revision/latest/scale-to-width-down/310?cb=20130523023812"></div>
-                               <br>`
-    productText.innerHTML = "";
-    picture.innerHTML = "";
-    reviews.innerHTML = "";
+                               <br>`;
+  productText.innerHTML = '';
+  picture.innerHTML = '';
+  reviews.innerHTML = '';
 }
