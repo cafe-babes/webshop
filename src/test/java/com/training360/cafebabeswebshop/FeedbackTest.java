51fabd6d5044568f587bd867d311f94d383dd570
Szabo Ruben <s.r.tailor777@gmail.com>
2019-03-29 09:33:12 +0100

    FeedbackTest giveAFeedback

--- src/test/java/com/training360/cafebabeswebshop/FeedbackTest.java
+++ src/test/java/com/training360/cafebabeswebshop/FeedbackTest.java
@@ -1,9 +1,12 @@
 package com.training360.cafebabeswebshop;
 import com.training360.cafebabeswebshop.category.Category;
+import com.training360.cafebabeswebshop.category.CategoryService;
 import com.training360.cafebabeswebshop.feedback.Feedback;
 import com.training360.cafebabeswebshop.feedback.FeedbackService;
 import com.training360.cafebabeswebshop.product.Product;
+import com.training360.cafebabeswebshop.product.ProductService;
 import com.training360.cafebabeswebshop.user.User;
+import com.training360.cafebabeswebshop.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,15 @@ public class FeedbackTest {
     @Autowired
     FeedbackService feedbackService;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ProductService productService;
+
 @Test
     public void testListFeedBacksByProductId(){
 
@@ -34,17 +46,21 @@ public class FeedbackTest {
 
 }
 
-//    @Test
-//    public void testGiveAFeedBack(){
-//
-//        User user = new User(5,"Ernő","alma@alma.com","ernőke","ernőő",
-//                1,"ROLE_ADMIN","ACTIVE");
-//        Category category = new Category(5,"balzsamecet",2);
-//        Product product = new Product(99,"sss","sssss","sssssssss","aaaaaa",
-//                500000,"ACTIVE",category);
-//        feedbackService.giveAFeedback(new Feedback(5,"Awesome!",5,user,
-//                product));
-//    }
+    @Test
+    public void testGiveAFeedBack(){
+
+        User exampleUser = userService.listUsers().get(0);
+        Product exampleproduct = productService.getProductById(1);
+
+        feedbackService.giveAFeedback(new Feedback(5,"Awesome!",5,exampleUser,
+                exampleproduct));
+
+        List<Feedback> feedbacks = feedbackService.listFeedBacksByProductId(exampleproduct .getId());
+
+        assertEquals("admin", feedbacks.get(0).getUser().getUserName());
+        assertEquals("Awesome!", feedbacks.get(0).getFeedback());
+
+    }
 
 
 }
