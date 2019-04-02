b14f4de89303331d4ec96b5e5e371581f44bcd89
ggerst <gerstg@gmail.com>
2019-04-02 11:56:55 +0200

    category test

--- src/main/java/com/training360/cafebabeswebshop/category/CategoryService.java
+++ src/main/java/com/training360/cafebabeswebshop/category/CategoryService.java
@@ -16,34 +16,31 @@ public class CategoryService {
     @Autowired
     CategoryDao categoryDao;
 
-    public List<Category> listCategories(){
+    public List<Category> listCategories() {
         return categoryDao.listCategories();
     }
 
     public long createCategoryAndGetId(Category category) throws DataAccessException {
         long max = categoryDao.getMaxOrdinal();
-        System.out.println(category.getOrdinal());
-        if (max+1<category.getOrdinal() || category.getOrdinal() < 0){
+        if (max + 1 < category.getOrdinal() || category.getOrdinal() < 0) {
             return -1;
         }
-        if(categoryDao.getCategoryNames().contains(category.getName())){
-            return -2;
-        }
-        if (category.getOrdinal()<=0){
+
+        if (category.getOrdinal() <= 0) {
             category.setOrdinal(max + 1);
-        }
-        else if (category.getOrdinal()<=max)
-            while (max >= category.getOrdinal()){
+        } else if (category.getOrdinal() <= max) {
+            while (max >= category.getOrdinal()) {
                 categoryDao.increaseOrdinal(max--);
             }
+        }
         return categoryDao.createCategoryAndGetId(category);
     }
 
-    public long getMaxOrdinal(){
+    public long getMaxOrdinal() {
         return categoryDao.getMaxOrdinal();
     }
 
-    public long getMinOrdinal(){
+    public long getMinOrdinal() {
         return categoryDao.getMinOrdinal();
     }
 
@@ -51,14 +48,14 @@ public class CategoryService {
         long ordinal = 0;
         List<Category> categories = categoryDao.listCategories();
 
-        for(Category c : categories){
-            if(c.getId() == id){
+        for (Category c : categories) {
+            if (c.getId() == id) {
                 ordinal = c.getOrdinal();
             }
         }
         categoryDao.deleteCategory(id);
-        for(Category category : categories){
-            if(category.getOrdinal() > ordinal){
+        for (Category category : categories) {
+            if (category.getOrdinal() > ordinal) {
                 categoryDao.decreaseOrdinal(category.getOrdinal());
             }
         }
@@ -73,35 +70,27 @@ public class CategoryService {
         List<Category> categories = categoryDao.listCategories();
         long originalOrdinal = category.getOrdinal();
 
-        for(Category c : categories) {
+        for (Category c : categories) {
             if (c.getId() == id) {
                 originalOrdinal = c.getOrdinal();
             }
         }
 
-        if(originalOrdinal > category.getOrdinal()){
-            for(int i = categories.size()-1; i >= 0; i--){
-                if(categories.get(i).getOrdinal() >= category.getOrdinal() && categories.get(i).getOrdinal() < originalOrdinal){
+        if (originalOrdinal > category.getOrdinal()) {
+            for (int i = categories.size() - 1; i >= 0; i--) {
+                if (categories.get(i).getOrdinal() >= category.getOrdinal() && categories.get(i).getOrdinal() < originalOrdinal) {
                     categoryDao.increaseOrdinal(categories.get(i).getOrdinal());
                 }
             }
             categoryDao.updateCategory(id, category);
         }
-        if(category.getOrdinal() > originalOrdinal){
-            for(int i = 0; i < categories.size(); i++){
-                if(categories.get(i).getOrdinal() <= category.getOrdinal() && categories.get(i).getOrdinal() > originalOrdinal){
+        if (category.getOrdinal() > originalOrdinal) {
+            for (int i = 0; i < categories.size(); i++) {
+                if (categories.get(i).getOrdinal() <= category.getOrdinal() && categories.get(i).getOrdinal() > originalOrdinal) {
                     categoryDao.decreaseOrdinal(categories.get(i).getOrdinal());
                 }
             }
             categoryDao.updateCategory(id, category);
-        }
-    }
-    public void writeBlob(Image image) {
-        try (FileInputStream in = new FileInputStream(new File(image.getFilename()))){
-            image.setBinary(in);
-            categoryDao.writeBlob(image);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
