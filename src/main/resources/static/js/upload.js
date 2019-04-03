577b94ce2a237527f0e5796bad5ca2305192d791
Paller Peter <pallerpet1402@gmail.com>
2019-04-03 16:22:38 +0200

    upload reserch

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -3,22 +3,19 @@ window.addEventListener('load', init);
 function init() {
     let uploadButton = document.getElementById('uploadButton');
     uploadButton.addEventListener('click', startUpload);
+    document.querySelector("#fileToUpload").addEventListener("change", function(){
+        document.querySelector("[for='"+ this.id +"']").innerText = this.files[0].name;
+    })
 }
 
 function startUpload() {
-    var s = window.location.href.split("=")[1];
-    console.log(s);
     let product_id = parseInt(window.location.href.split("=")[1]);
-    console.log(product_id);
-
     let file = document.getElementById('fileToUpload').files[0];
     const formData = new FormData();
 
     formData.append('file', file);
     formData.append('productId', product_id);
 
-
-    console.log(formData);
     fetch('/image', {
         method: 'POST',
         body: formData
@@ -34,32 +31,32 @@ function startUpload() {
 }
 
 
-function bs_input_file() {
-	$(".input-file").before(
-		function() {
-			if ( ! $(this).prev().hasClass('input-ghost') ) {
-				var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
-				element.attr("name",$(this).attr("name"));
-				element.change(function(){
-					element.next(element).find('input').val((element.val()).split('\\').pop());
-				});
-				$(this).find("button.btn-choose").click(function(){
-					element.click();
-				});
-				$(this).find("button.btn-reset").click(function(){
-					element.val(null);
-					$(this).parents(".input-file").find('input').val('');
-				});
-				$(this).find('input').css("cursor","pointer");
-				$(this).find('input').mousedown(function() {
-					$(this).parents('.input-file').prev().click();
-					return false;
-				});
-				return element;
-			}
-		}
-	);
-}
-$(function() {
-	bs_input_file();
-});
+//function bs_input_file() {
+//	$(".input-file").before(
+//		function() {
+//			if ( ! $(this).prev().hasClass('input-ghost') ) {
+//				var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+//				element.attr("name",$(this).attr("name"));
+//				element.change(function(){
+//					element.next(element).find('input').val((element.val()).split('\\').pop());
+//				});
+//				$(this).find("button.btn-choose").click(function(){
+//					element.click();
+//				});
+//				$(this).find("button.btn-reset").click(function(){
+//					element.val(null);
+//					$(this).parents(".input-file").find('input').val('');
+//				});
+//				$(this).find('input').css("cursor","pointer");
+//				$(this).find('input').mousedown(function() {
+//					$(this).parents('.input-file').prev().click();
+//					return false;
+//				});
+//				return element;
+//			}
+//		}
+//	);
+//}
+//$(function() {
+//	bs_input_file();
+//});
