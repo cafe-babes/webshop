ccb1f155c514319bcd5163c7c4e15d1f1777a06a
ggerst <gerstg@gmail.com>
2019-04-10 00:11:45 +0200

    autowired mentesítés

--- src/main/java/com/training360/cafebabeswebshop/report/ReportController.java
+++ src/main/java/com/training360/cafebabeswebshop/report/ReportController.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ReportController {
 
     private ReportService reportService;
-dd .
+
     public ReportController(ReportService reportService) {
         this.reportService = reportService;
     }
